4e51122f17ed2864b2a8da845f0fff87b7b36750
Golden1636 <okeamagolden@yahoo.com>
2025-09-21 15:52:59 +0100

    got the save input button to take the value of the input field and render it out in an unordered list, then clear the input field

---
+++ index.js
@@ -0,0 +1,19 @@
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
+let myLeads = [];
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  renderLeads();
+});
+
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li>" + myLeads[i] + "</li>";
+  }
+  ulEl.innerHTML = listItems;
+}
