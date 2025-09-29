32ec11e5bf49e4359b882e6b361067bd637e2b71
Golden1636 <okeamagolden@yahoo.com>
2025-09-29 16:07:45 +0100

    added a manifest.json file and an icon.png file, then deployed the app through chrome://extensions

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
-
-let myLeads = [];
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
@@ -12,8 +11,16 @@ inputBtn.addEventListener("click", function () {
 
 function renderLeads() {
   let listItems = "";
+
   for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>";
+    listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `;
   }
+
   ulEl.innerHTML = listItems;
 }
